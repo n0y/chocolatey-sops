f13d6d17feec41eb3ed73ce3abd65abe9cfcfac0
Bertram Feÿ <bertram.fey@deutschebahn.com>
2025-03-20 15:09:27 +0100

    chore: Build each package in a separate project: this one is the SOPS packaging chore: enable Renovate updates for this project

---
+++ build-packagespec.ps1
@@ -0,0 +1,18 @@
+$Props = convertfrom-stringdata (get-content versions.properties | Select-String -pattern "^#" -NotMatch)
+$SopsVersion = $Props.UPSTREAM_VERSION
+"Building Upstream Version: $SopsVersion"
+""
+
+$ChecksumResponse = Invoke-WebRequest -Uri "https://github.com/getsops/sops/releases/download/v$SopsVersion/sops-v$SopsVersion.checksums.txt"
+$SopsChecksum = (($ChecksumResponse.tostring() -split "[`r`n]" | Select-String "sops-v$SopsVersion.exe" | select -First 1) -split " ")[0].ToUpper()
+
+"Discovered Checksum: $SopsChecksum"
+
+if (Test-Path -LiteralPath .\target) {
+    Remove-Item -LiteralPath .\target -Recurse
+}
+New-Item -ItemType Directory -Force -Path .\target\tools | Out-Null
+
+(Get-Content .\src\tools\chocolateyinstall.template.ps1) -replace '%%VERSION%%', $SopsVersion -replace '%%CHECKSUM%%', $SopsChecksum | Out-File -Encoding utf8 ".\target\tools\chocolateyinstall.ps1"
+(Get-Content .\src\sops.template.nuspec) -replace '%%VERSION%%', $SopsVersion -replace '%%CHECKSUM%%', $SopsChecksum | Out-File -Encoding utf8 ".\target\sops.nuspec"
+
